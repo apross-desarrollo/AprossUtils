69a344ba47166af691fa4b3cd634c668bb9a90c5
Fernando Hidalgo <hidalgofernandojavier@gmail.com>
2021-10-21 10:58:18 -0300

    ADD FileTypeAttribute

---
+++ HtmlHelpers/FileTypeAttribute.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AprossUtils.HtmlHelpers
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class FileTypeAttribute : ValidationAttribute, IClientValidatable
+    {
+        private const string _DefaultErrorMessage = "Sólo Archivos: {0}";
+        private IEnumerable<string> _ValidTypes { get; set; }
+
+        public FileTypeAttribute(string validTypes)
+        {
+            _ValidTypes = validTypes.Split(',').Select(s => s.Trim().ToLower());
+            ErrorMessage = string.Format(_DefaultErrorMessage, string.Join(" or ", _ValidTypes));
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            IEnumerable<HttpPostedFileBase> files = value as IEnumerable<HttpPostedFileBase>;
+            if (files != null)
+            {
+                foreach (HttpPostedFileBase file in files)
+                {
+                    if (file != null && !_ValidTypes.Any(e => file.FileName.EndsWith(e)))
+                    {
+                        return new ValidationResult(ErrorMessageString);
+                    }
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            var rule = new ModelClientValidationRule
+            {
+                ValidationType = "filetype",
+                ErrorMessage = ErrorMessageString
+            };
+            rule.ValidationParameters.Add("validtypes", string.Join(",", _ValidTypes));
+            yield return rule;
+        }
+
+        public void Render(ViewContext viewContext, TextWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class FileSizeAttribute : ValidationAttribute, IClientValidatable
+    {
+        private const string _DefaultErrorMessage = "Tamaño Máximo: {0} MB";
+        private int _MaxSize;
+
+        public FileSizeAttribute(int max_size = 5)
+        {
+            _MaxSize = max_size;
+            ErrorMessage = string.Format(_DefaultErrorMessage, _MaxSize);
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            IEnumerable<HttpPostedFileBase> files = value as IEnumerable<HttpPostedFileBase>;
+            if (files != null)
+            {
+                foreach (HttpPostedFileBase file in files)
+                {
+                    if (file != null && file.ContentLength > _MaxSize * 1024 * 1024)
+                    {
+                        return new ValidationResult(ErrorMessageString);
+                    }
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            var rule = new ModelClientValidationRule
+            {
+                ValidationType = "filesize",
+                ErrorMessage = ErrorMessageString
+            };
+            rule.ValidationParameters.Add("maxsize", _MaxSize);
+            yield return rule;
+        }
+    }
+}
--- StringUtils.cs
+++ StringUtils.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
 
 namespace AprossUtils.StringUtils
 {
@@ -37,6 +37,28 @@ namespace AprossUtils.StringUtils
                             d[i - 1, j - 1] + 1 //a substitution
                             );
             return d[n, m];
+        }
+    }
+    static public class Slug
+    {
+
+        public static string GenerateSlug(this string phrase)
+        {
+            string str = phrase.RemoveAccent().ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+
+        public static string RemoveAccent(this string txt)
+        {
+            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return System.Text.Encoding.ASCII.GetString(bytes);
         }
 
     }
