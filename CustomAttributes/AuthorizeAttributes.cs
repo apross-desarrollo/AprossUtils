684c6fd097c2d4d93b963fad510135de19bc6f2c
Fernando Hidalgo <hidalgofernandojavier@gmail.com>
2022-01-26 18:44:26 -0300

    FEATURE: AuthorizeAttributes

---
+++ CustomAttributes/AuthorizeAttributes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AprossUtils.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AuthorizeWidgetAttribute : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(new
+         RouteValueDictionary(new { controller = "Home", action = "WidgetError", area="" }));
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AuthorizeNoRedirectAttribute : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new HttpForbiddenResult();
+        }
+    }
+
+    public class HttpForbiddenResult : HttpStatusCodeResult
+    {
+        public HttpForbiddenResult()
+            : this(null)
+        {
+        }
+
+        public HttpForbiddenResult(string statusDescription)
+            : base(HttpStatusCode.Forbidden, statusDescription)
+        {
+        }
+    }
+
+}
