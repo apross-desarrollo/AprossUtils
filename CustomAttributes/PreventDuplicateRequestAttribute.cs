896b7755ac29a0187812896895e819c56b666d4e
Fernando Hidalgo <hidalgofernandojavier@gmail.com>
2023-04-21 14:37:12 -0300

    Prevent Duplicate Requests

---
+++ CustomAttributes/PreventDuplicateRequestAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web;
+
+namespace AprossUtils.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class PreventDuplicateRequestAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (HttpContext.Current.Request["__RequestVerificationToken"] == null)
+                return;
+
+            var currentToken = HttpContext.Current.Request["__RequestVerificationToken"].ToString();
+
+            if (HttpContext.Current.Session["LastProcessedToken"] == null)
+            {
+                HttpContext.Current.Session["LastProcessedToken"] = currentToken;
+                return;
+            }
+
+            lock (HttpContext.Current.Session["LastProcessedToken"])
+            {
+                var lastToken = HttpContext.Current.Session["LastProcessedToken"].ToString();
+
+                if (lastToken == currentToken)
+                {
+                    filterContext.Controller.ViewData.ModelState.AddModelError("", "Ésta solitud ya fue enviada, chequee el estado de la transacción");
+                    return;
+                }
+
+                HttpContext.Current.Session["LastProcessedToken"] = currentToken;
+            }
+        }
+    }
+}
